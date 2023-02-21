902c7ef3f47ac85bc7b049df9291713230bb35a8
Esochi <sochiera.emilia@gmail.com>
2023-02-21 20:29:42 +0100

    no message

--- interface.ts
+++ interface.ts
@@ -2,14 +2,20 @@ class MyAwesomeClass{
 
 scream() : void {
     console.log("AAAAAAAAAAAAAAAA");
+
+    additionalScream();
 }
 
 roar() : void {
     console.log("RRRRRRRRRRRRRRRRRRRRRR")
+
+    additionalRoar();
 }
 
 shout() : void {
 console.log("ARGHHH!!!!!");
+
+additionalShoutFunctionallity();
 }
 
 }
